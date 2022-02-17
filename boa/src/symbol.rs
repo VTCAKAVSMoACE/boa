cdb7d38753cc7421423dce3cdbcb6840ffaf0f9b
jedel1043 <jedel0124@gmail.com>
2022-02-17 14:43:10 -0600

    Fix rust tests

--- boa/src/symbol.rs
+++ boa/src/symbol.rs
@@ -34,7 +34,7 @@ use std::{
 ///# use boa::symbol::WellKnownSymbols;
 ///
 /// let iterator = WellKnownSymbols::iterator();
-/// assert_eq!(iterator.description().as_deref(), Some("Symbol.iterator"));
+/// assert_eq!(iterator.description().unwrap().as_std_string_lossy(), "Symbol.iterator");
 /// ```
 /// This is equivalent to `let iterator = Symbol.iterator` in JavaScript.
 #[derive(Debug, Clone)]
